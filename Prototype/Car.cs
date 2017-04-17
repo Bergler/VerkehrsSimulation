9edb256bb8e91c00f8d1fe74385f634ef2a4cef6
Hannes Karl Kleiner <hkleiner.its-m2016@fh-salzburg.ac.at>
2017-04-17 17:45:47 +0200

    first commit

---
+++ Prototype/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Prototype
+{
+    class Car
+    {
+        private Rectangle car;
+        private int speed;
+        private double x, y;
+
+
+        public Car(Canvas canv, int _speed)
+        {
+            speed = _speed;
+            car = new Rectangle();
+            car.Height = 40;
+            car.Width = 50;
+            car.Fill = new SolidColorBrush(Colors.Black);
+            canv.Children.Add(car);
+            Canvas.SetTop(car, 160);
+            Canvas.SetLeft(car, 0);
+        }
+
+        public void moveNext()
+        {
+            Canvas.SetLeft(car, Canvas.GetLeft(car)+speed);
+        }
+
+
+    }
+}
---
+++ Prototype/Street.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Prototype
+{
+    class Street
+    {
+        private Rectangle rect;
+        private double x1, y1, x2, y2;
+        private char heading;
+        private int speedlimit;
+        List<Car> cars;
+        private Canvas canvas;
+
+        private Street nextelem;
+        private Street prevelem;
+
+        public Street(double x1, double y1, double x2,double y2,Canvas canv)
+        {
+            canvas = canv;
+            rect = new Rectangle();
+            rect.Height = 100;
+            rect.Width = 500;
+            rect.Fill = new SolidColorBrush(Colors.Gray);
+            canv.Children.Add(rect);
+            Canvas.SetLeft(rect, 0);
+            Canvas.SetTop(rect, 150);
+            cars = new List<Car>();
+    
+        }
+
+        public void updateAll()
+        {
+            foreach(Car car in cars)
+            {
+                car.moveNext();
+            }
+        }
+
+        public void addCar(int speed)
+        {
+            cars.Add(new Car(canvas,speed));
+        }
+
+
+    }
+}
